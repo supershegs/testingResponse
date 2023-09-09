c8f09206665063eaa17803da6cbf7e9444c7b59c
supershegs <segsamos@gmail.com>
2023-09-09 15:56:25 +0100

    utc check

--- app/c_date.py
+++ app/c_date.py
@@ -1,12 +1,29 @@
 import datetime
-import pytz
 
-current_date = datetime.datetime.now()
+from datetime import datetime
+# import pytz
+
+# current_date = datetime.datetime.now()
+
+current_date = datetime.now()
 current_day_of_week = current_date.strftime("%A")
 
-utc_timezone = pytz.timezone('UTC')
-current_utc_time = datetime.datetime.now(utc_timezone)
-formatted_utc_time = current_utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+# utc_timezone = pytz.timezone('UTC')
+# current_utc_time = datetime.datetime.now(utc_timezone)
+# formatted_utc_time = current_utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-# print("Current Day of the Week:", current_day_of_week, current_date, current_utc_time)
-# print(formatted_utc_time)
+
+
+current_utc_time = datetime.utcnow()
+formatted_utc_time = current_utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+
+print("Current Day of the Week:", current_day_of_week)
+print(current_date)
+print('utc: ',current_utc_time)
+print('utc 1:',formatted_utc_time) #utc 1: 2023-09-09T13:43:27Z
+
+
+
+
